98a56ccb517fafed2938271df809e7050c485e90
Joseph Dureau <joseph.dureau@snips.ai>
2017-12-21 16:21:53 +0100

    more intents

--- snipsbabycare/snipsbabycare.py
+++ snipsbabycare/snipsbabycare.py
@@ -4,6 +4,16 @@
 import requests
 import json
 
+def load_state():
+    with open('state.json') as data_file:    
+        state = json.load(data_file)
+    return state
+        
+def write_state(state):
+    with open('state.json', 'w') as outfile:
+        json.dump(state, outfile)
+
+
 class SnipsBabyCare:
     """ Baby Care skill for Snips. """
 
@@ -13,6 +23,7 @@ class SnipsBabyCare:
         :param ifttt_key: IFTTT Key
         """
         self.ifttt_key = ifttt_key
+
 
     def send_row(self, action, value=None):
         """ Log an entry for allaitement for [duration] minutes. """
@@ -24,6 +35,27 @@ class SnipsBabyCare:
             data = {"value1": action, "value2": value})
 
 
+    def log_breast(self, side=None):
+        if side is not None:
+            state = load_state()
+            state["side"] = side
+            write_state(state)
+
+
+    def last_breast(self):
+        state = load_state()
+        return state.get("side", "")
+
+    def next_breast(self):
+        state = load_state()
+        side = state.get("side", None)
+        if side is "gauche":
+            return "droite"
+        elif side is "droite":
+            return "gauche"
+        else:
+            return ""
+        
 if __name__ == "__main__":
     sbc = SnipsBabyCare()
     # sh.light_on_set("gold", 42, "Bedroom")
